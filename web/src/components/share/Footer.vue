881484e3122f80a77b6f7de04c9ce12325e7abda
Tolga Gunaydin <gnydnntolga@gmail.com>
2019-08-20 21:37:45 +0300

    Footer Tasarlandı

--- web/src/components/share/Footer.vue
+++ web/src/components/share/Footer.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>
-    <h2> Footer </h2>
+  <div class="footer py-5">
+    <div class="container py-4">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-12">
+          <div class="footerİmg text-center">
+            <img src="@/assets/images/logo.png" class="img-fluid">
+          </div>
+            <div class="text py-3">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-8 col-md-8 col-sm-8 col-12 py-3 schollList">
+          <div class="row justify-content-center">
+              <div class="col-lg-3 col-md-3 col-sm-3 col-12">
+                  <p> Anaokulları </p>
+              </div>
+              <div class="col-lg-3 col-md-3 col-sm-3 col-12">
+                  <p> İlkokullar </p>
+              </div>
+              <div class="col-lg-3 col-md-3 col-sm-3 col-12">
+                  <p> Ortaokullar </p>
+              </div>
+              <div class="col-lg-3 col-md-3 col-sm-3 col-12">
+                  <p> Liseler </p>
+              </div>
+          </div>
+        </div>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-12 copyright">
+            <p> © Copyright 2019  Tüm hakları saklıdır. </p>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
